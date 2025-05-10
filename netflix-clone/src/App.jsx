19f3ef086b5c04fd746b079da84ddd57645b9df5
kingaddis <66341899+kingaddis@users.noreply.github.com>
2025-05-09 21:29:27 -0400

    header and footer

--- netflix-clone/src/App.jsx
+++ netflix-clone/src/App.jsx
@@ -1,10 +1,13 @@
+import Home from "./Pages/Home/Home"
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-<h1>Hello</h1>
+        <Home/>
+
+ 
     </div>
   );
 }
---
+++ netflix-clone/src/Pages/Home/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Footer from "../../Components/Footer/Footer"
+import Header from "../../Components/Header/Header"
+
+function Home() {
+  return (
+    <>
+
+    <Header/>
+    <Footer/>
+    
+    </>
+
+
+
+  )
+}
+
+export default Home
